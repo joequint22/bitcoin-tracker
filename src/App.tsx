4cb4100ca5de295c42625e99da29fb9e02f6e9c6
joseph quintiliano <jquintiliano1@gmail.com>
2023-11-25 00:52:09 -0500

    row structure & animaiton

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-import { AmountInput } from './components/AmountInput'
-import ResultRow from './components/ResultRow'
+import { useState } from "react";
+import "./App.css";
+import { AmountInput } from "./components/AmountInput";
+import ResultRow from "./components/ResultRow";
 
 function App() {
-  const [amount, setAmount] = useState('')
+  const [amount, setAmount] = useState("");
 
   return (
     <>
-    <div id="container" className='h-screen bg-[#113] text-white bg-gradient-radial from-[#272761] via-[#1e1e58] to-[#113]'> 
-
-     <div  className='max-w-2xl mx-auto px-4 py-8'>
-      <h1 className='uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%'>Find Cheapest BTC</h1>
-     </div>
-     <div className='flex justify-center mt-6'>
-        <AmountInput
-          placeholder="Amount"
-          value={amount}
-          onChange={e => setAmount(e.target.value)}
-        />
-     </div>
-     <div className='mt-6'>
-       <ResultRow />
-       <ResultRow />
-       <ResultRow />
-       <ResultRow />
-     </div>
-    </div>
+      <div
+        id="container"
+        className="h-screen w-full bg-[#113] text-white bg-gradient-radial from-[#272761] via-[#1e1e58] to-[#113] mx-auto px-4 py-8"
+      >
+        <div className="">
+          <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%">
+            Find Cheapest BTC
+          </h1>
+        </div>
+        <div className="flex justify-center mt-6">
+          <AmountInput
+            placeholder="Amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <div className="mt-6">
+          <ResultRow />
+          <ResultRow />
+          <ResultRow />
+          <ResultRow loading={true}/>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
- 
+export default App;
--- src/components/ResultRow.tsx
+++ src/components/ResultRow.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 
+type ResultRowProps = {
+    loading?: boolean
+}
 
 
-export default function ResultRow() {
+export default function ResultRow({ loading }: ResultRowProps) {
   return (
     <>
-    <div className='border min-h-12 border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2'>
+    <div className='relative overflow-hidden border min-h-12 border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2'>
         <div className='flex gap-4'>
             <div>logo</div>
-            //grow will make this div take up all of the space between the next div
+            {/* //grow will make this div take up all of the space between the next div */}
             <div className='grow'>providers name</div>
-            <div>
-                <span>0.001</span>
-                <span>BTC</span>
+            <div className='flex gap-2'>
+                <span className='text-xl text-purple-200/80'>0.000</span>
+                <span className='text-xl text-purple-300/50'>BTC</span>
             </div>
+            {loading && (
+                <div className='inset-0 absolute bg-gradient-to-r from-transparent via-blue-800/50 to-transparent skeleton-animation'/>
+            )}
         </div>
     </div>
     </>
