477991b8d71349a21aa424a7d81296860ab016f9
joseph quintiliano <jquintiliano1@gmail.com>
2023-11-25 16:50:05 -0500

    implemented backend / fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { AmountInput } from "./components/AmountInput";
 import ResultRow from "./components/ResultRow";
+import axios from 'axios'
+
+type CachedResults = {
+  provider: string
+  btc: string
+}
 
 function App() {
   const [amount, setAmount] = useState("");
+  const [cachedResults, setCachedResults] = useState<CachedResults[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+ 
+  useEffect(() => {
+    axios.get('https://sz6tcer3ng.us.aircode.run/cachedValues')
+    .then( res => {
+         setCachedResults(res.data.results)
+         setIsLoading(false)
+
+       })
+  }, []);
+
 
   return (
     <>
       <div
         id="container"
-        className="h-screen w-full bg-[#113] text-white bg-gradient-radial from-[#272761] via-[#1e1e58] to-[#113] mx-auto px-4 py-8"
+        className="h-screen bg-[#113] text-white bg-gradient-radial from-[#272761] via-[#1e1e58] to-[#113]"
       >
-        <div className="">
+        <div className="mx-auto px-4 py-8 max-w-2xl ">
           <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%">
             Find Cheapest BTC
           </h1>
-        </div>
-        <div className="flex justify-center mt-6">
-          <AmountInput
-            placeholder="Amount"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-        </div>
-        <div className="mt-6">
-          <ResultRow />
-          <ResultRow />
-          <ResultRow />
-          <ResultRow loading={true}/>
+          <div className="flex justify-center mt-6">
+            <AmountInput
+              placeholder="Amount"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              />
+          </div>
+          <div className="mt-6 ">
+            {isLoading && (
+              <>
+                <ResultRow loading={true} />
+                <ResultRow loading={true} />
+                <ResultRow loading={true} />
+                <ResultRow loading={true} />
+              </>
+            )}
+            {!isLoading && cachedResults.map((result, key) => (
+              <ResultRow key={key} providerName={result.provider}/>
+            )) }
+          </div>
         </div>
       </div>
     </>
--- src/components/ResultRow.tsx
+++ src/components/ResultRow.tsx
@@ -2,21 +2,35 @@ import React from 'react'
 
 type ResultRowProps = {
     loading?: boolean
+    providerName?: string
+    btc?: string
 }
 
 
-export default function ResultRow({ loading }: ResultRowProps) {
+export default function ResultRow({ 
+    loading, providerName, btc
+ }: ResultRowProps) {
   return (
-    <>
-    <div className='relative overflow-hidden border min-h-12 border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2'>
+    <> 
+    <div className='relative overflow-hidden border h-[64px] border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2'>
         <div className='flex gap-4'>
-            <div>logo</div>
+            {providerName && (
+                <>
+                <div>logo</div> 
+                </>
+            )}
             {/* //grow will make this div take up all of the space between the next div */}
-            <div className='grow'>providers name</div>
-            <div className='flex gap-2'>
-                <span className='text-xl text-purple-200/80'>0.000</span>
-                <span className='text-xl text-purple-300/50'>BTC</span>
-            </div>
+            <div className='grow'>{providerName || ''}</div>
+            {btc && (
+                <>
+                <div className='flex gap-2'>
+                    <span className='text-xl text-purple-200/80'>{btc}</span>
+                    <span className='text-xl text-purple-300/50'>BTC</span>
+                </div> 
+                </>
+            )
+
+            }
             {loading && (
                 <div className='inset-0 absolute bg-gradient-to-r from-transparent via-blue-800/50 to-transparent skeleton-animation'/>
             )}
